6eb83d3f4237363443bc744261c720c5b2203d33
Moon-seok-ho <93657697+Moon-seok-ho@users.noreply.github.com>
2023-01-25 10:51:35 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title("Avocado Prices dashboard")
+
+st.markdown('''
+# Avocado Prices dashboard
+
+테스트용 대시보드를 만들자
+Data source: [kaggle](https://www.kaggle.com/datasets/timmate/avocado-prices-2020)
+''')
+
+avocado = pd.read_csv("avocado.csv")
+
+table = avocado.groupby("type")[['total_volume','average_price']].mean()
+
+st.dataframe(table)
+
+selected_geo = st.selectbox(label="Geography", options=avocado['geography'].unique())
+
+submitted = st.button("Submit")
+
+if submitted:
+    line_fig = px.line(avocado[avocado['geography']==selected_geo], x='date', y='average_price',
+    color='type', title=f'{selected_geo} dashboard'
+    )
+
+    st.plotly_chart(line_fig)
